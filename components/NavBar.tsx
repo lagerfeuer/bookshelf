c048d4fb5e9f1a3fd70a2455114f878f116cea0a
Lukas Deutz <github@deutz.io>
2023-01-31 22:33:22 -0500

    Add link to Github

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,7 +3,22 @@ type Props = { title: string };
 export default function NavBar({ title }: Props) {
   return (
     <div className="navbar bg-neutral text-neutral-content">
-      <a className="btn btn-ghost normal-case text-xl">{title}</a>
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">{title}</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a
+              href="https://github.com/lagerfeuer/bookshelf"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
