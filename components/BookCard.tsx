deba1ce4ceed1b9b178a9dad0e2f93455c111563
Lukas Deutz <github@deutz.io>
2023-02-04 11:05:05 -0500

    Add ability to copy ISBN

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,9 +1,9 @@
 import { ISBN } from "@/helper/types";
-type Props = { title: string; isbn: ISBN; authors: string[]; cover: string };
+type Props = { title: string; isbn: ISBN; authors: string[]; cover: string, onClick: () => void };
 
-export default function BookCard({ title, authors, cover, isbn }: Props) {
+export default function BookCard({ title, authors, cover, isbn, onClick }: Props) {
   return (
-    <div className="transition ease-in-out hover:scale-110">
+    <div className="transition ease-in-out hover:scale-110" onClick={onClick}>
       <img src={cover} alt="Book cover" className="object-cover" />
     </div>
   );
--- components/BookShelf.tsx
+++ components/BookShelf.tsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import BookCard from "./BookCard";
 import type { BookInfo } from "@/helper/api";
-import type { Nullable, ISBN } from "@/helper/types";
+import type { ISBN } from "@/helper/types";
 
 type Books = { [key: string]: { [key: string]: BookInfo } };
 type BookShelfProps = { books: Books };
@@ -8,6 +9,17 @@ type BookShelfProps = { books: Books };
 const yearSort = (a: string, b: string) => (a < b ? 1 : -1);
 
 export default function BookShelf({ books }: BookShelfProps) {
+  const [toastVisible, setToastVisible] = useState(false);
+
+  function showToast(): void {
+    console.log("Show Toast")
+    setToastVisible(true);
+    setTimeout(() => {
+      console.log("Hide Toast")
+      setToastVisible(false);
+    }, 3000);
+  }
+
   return (
     <div className="pb-8">
       {Object.keys(books)
@@ -25,12 +37,25 @@ export default function BookShelf({ books }: BookShelfProps) {
                     authors={book.authors}
                     cover={book.cover}
                     isbn={isbn}
+                    onClick={() => {
+                      navigator.clipboard.writeText(isbn.toString());
+                      showToast();
+                    }}
                   />
                 );
               })}
             </div>
           </div>
         ))}
+      <div hidden={!toastVisible}>
+        <div className="toast">
+          <div className="alert alert-info">
+            <div>
+              <span>Copied ISBN to clipboard!</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
